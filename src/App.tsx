e09ab6d901dcd874a37a2689aad09c5f5a0aca13
Onwuzor Victor <victor.onwuzor@andela.com>
2020-02-14 09:48:58 +0100

    add developer profile section

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,34 @@ const App: React.FC = () => {
   return (
     <div className="layout">
       <Header />
+      <main>
+        <section className="section-profile">
+          <div className="row">
+            <div className="col-6 u-margin-bottom-small">
+              <div className="profile__box">
+                <div className="profile__image"></div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="profile__detail">
+                <h4 className="heading-sub">FullStack Software Engineer</h4>
+                <h1 className="heading-primary">Victor Onwuzor</h1>
+                <p className="paragraph">
+                  Experienced Software Engineer with a passion for developing innovative solutions that
+                  expedite the efficiency and effectiveness of organizational success. Well-versed in technology
+                  and writing code to create systems that are reliable and user-friendly.
+                  A skilled leader who has the proven ability to motivate, educate, and manage a team of
+                  professionals to build software programs and effectively track changes.
+              </p>
+                <div className="u-margin-top-small">
+                  <a href="#" className="btn btn__primary">Download CV</a>
+                  <a href="#" className="btn btn__secondary">Contact</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,8 +6,37 @@ const Header = () => (
         <div className="header__logo-box">
             <Link to={"#"}>
                 <div className="header__logo-symbol">V</div>
-                <div className="header__logo-text">Victor <span>Onwuzor</span></div>
+                <div className="header__logo-text">
+                    Victor <span>Onwuzor</span>
+                </div>
             </Link>
+        </div>
+
+        <div className="header__navigation">
+            {/* <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
+            <label className="navigation__button">
+                <span className="navigation__icon"></span>
+            </label>
+            <div className="navigation__background"></div> */}
+            <nav className="header__navigation-nav">
+                <ul className="header__navigation-list">
+                    <li className="header__navigation-item">
+                        <a href="#" className="header__navigation-link">About Me</a>
+                    </li>
+                    <li className="header__navigation-item">
+                        <a href="#" className="header__navigation-link">Resume</a>
+                    </li>
+                    <li className="header__navigation-item">
+                        <a href="#" className="header__navigation-link">Portfolio</a>
+                    </li>
+                    <li className="header__navigation-item">
+                        <a href="#" className="header__navigation-link">Blog</a>
+                    </li>
+                    <li className="header__navigation-item">
+                        <a href="#" className="header__navigation-link">Contact</a>
+                    </li>
+                </ul>
+            </nav>        
         </div>
     </header>
 );
