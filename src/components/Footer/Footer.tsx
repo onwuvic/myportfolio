df4d61b27d022df080d99e47804c0c76f2b73637
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-09-11 23:17:07 +0100

    add social media links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter, faLinkedinIn, faMediumM } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => (
     <footer className="footer">
         <div className="footer__social">
             <ul className="footer__social-list">
                 <li className="footer__social-item">
-                    <Link to={"#"} className="footer__social-link">Twitter</Link>
+                    <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/victoronwuzor" className="footer__social-link">
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </a>
                 </li>
                 <li className="footer__social-item">
-                    <Link to={"#"} className="footer__social-link">LinkedIn</Link>
+                    <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/victoronwuzor" className="footer__social-link">
+                     <FontAwesomeIcon icon={faLinkedinIn} />
+                    </a>
                 </li>
                 <li className="footer__social-item">
-                    <Link to={"#"} className="footer__social-link">GitHub</Link>
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/onwuvic" className="footer__social-link">
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a>
                 </li>
                 <li className="footer__social-item">
-                    <Link to={"#"} className="footer__social-link">Medium</Link>
-                </li>
-                <li className="footer__social-item">
-                    <Link to={"#"} className="footer__social-link">Instagram</Link>
-                </li>
-                <li className="footer__social-item">
-                    <Link to={"#"} className="footer__social-link">Email</Link>
+                    <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@victoronwuzor" className="footer__social-link">
+                        <FontAwesomeIcon icon={faMediumM} />
+                    </a>
                 </li>
             </ul>
         </div>
