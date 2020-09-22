92fd7df9819be0ac0e7b43c8f9e4fc30c19ec8db
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-09-22 09:35:26 +0100

    add real resume data

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -7,10 +7,12 @@ import TimelineItem from '../Timeline/TimelineItem';
 import Container from '../Container/Container';
 import PageHeader from '../PageHeader/PageHeader';
 import Section from '../Section/Section';
+import * as data from '../../data/index.json';
+import Skill from '../Skill/Skill';
 
 const Resume = () => (
     <Section>
-        <PageHeader title="Resume" subTitle="3 Years of Experience" />
+        <PageHeader title="Resume" subTitle="4 Years of Experience" />
 
         <Container>
             <GridContainer>
@@ -18,11 +20,11 @@ const Resume = () => (
                     <HeadingSecondary text="Experience" />
 
                     <Timeline>
-                        {/* provide data on experience here and loop through them */}
-                        <TimelineItem />
-                        <TimelineItem />
-                        <TimelineItem />
-                        <TimelineItem />
+                        {
+                            data.experiences.map(experience => {
+                                return <TimelineItem {...experience} />
+                            })
+                        }
                     </Timeline>
                 </GridItem>
 
@@ -30,13 +32,15 @@ const Resume = () => (
                     <HeadingSecondary text="Education" />
 
                     <Timeline>
-                        {/* provide data on enducation here and loop through them. Make p optional*/}
-                        <div className="timeline__item">
-                            <h5 className="timeline__period">2015</h5>
-                            <span className="timeline__text">University of Benin</span>
-                            <h4 className="heading-tertiary">Bachelor of Science in Economics and Statistics</h4>
-                            {/* <p className="paragraph">Lorem ipsum, tur illum perferendis suscipit neque sit!</p> */}
-                        </div>
+                        {
+                            data.educations.map(education => {
+                                return <div className="timeline__item">
+                                        <h5 className="timeline__period">{education.year}</h5>
+                                        <span className="timeline__text">{education.school}</span>
+                                        <h4 className="heading-tertiary">{education.degree}</h4>
+                                    </div>
+                            })
+                        }
                     </Timeline>
                 </GridItem>
 
@@ -46,46 +50,19 @@ const Resume = () => (
                 <GridItem styles="col-4">
                     <HeadingSecondary text="Basic Stack" />
 
-                    {/* Make this a component skills */}
-                    <div className="skills">
-                        <ul>
-                            <li>JavaScript</li>
-                            <li>TypeScript</li>
-                            <li>Node.Js</li>
-                            <li>HTML5</li>
-                            <li>CSS3 / SASS</li>
-                        </ul>
-                    </div>
+                    <Skill skills={data.skills.basics} />
                 </GridItem>
 
                 <GridItem styles="col-4">
                     <HeadingSecondary text="Framework/Library" />
 
-                    <div className="skills">
-                        <ul>
-                            <li>Express.JS</li>
-                            <li>Sails.JS</li>
-                            <li>Nest.JS</li>
-                            <li>Angular 2+</li>
-                            <li>React / Redux</li>
-                            <li>Ngrx</li>
-                            <li>RxJS</li>
-                        </ul>
-                    </div>
+                    <Skill skills={data.skills.libraries} />
                 </GridItem>
 
                 <GridItem styles="col-4">
                     <HeadingSecondary text="Database" />
 
-                    <div className="skills">
-                        <ul>
-                            <li>MongoDB / Mongoose</li>
-                            <li>PostgreSQL / Sequelize</li>
-                            <li>MySQL</li>
-                            <li>Redis</li>
-                            <li>SQL</li>
-                        </ul>
-                    </div>
+                    <Skill skills={data.skills.databases} />
                 </GridItem>
             </GridContainer>
 
@@ -93,41 +70,27 @@ const Resume = () => (
                 <GridItem styles="col-4">
                     <HeadingSecondary text="Testing Tools" />
 
-                    <div className="skills">
-                        <ul>
-                            <li>Mocha / Chai</li>
-                            <li>Jest</li>
-                            <li>Jasmine</li>
-                            <li>Protractor</li>
-                            <li>Cypress</li>
-                            <li>Selenium</li>
-                            <li>Enzyme</li>
-                        </ul>
-                    </div>
+                    <Skill skills={data.skills.testing} />
                 </GridItem>
 
                 <GridItem styles="col-4">
                     <HeadingSecondary text="Continuous Integration" />
 
-                    <div className="skills">
-                        <ul>
-                            <li>Circle CI</li>
-                            <li>Travis CI</li>
-                            <li>Code Climate</li>
-                        </ul>
-                    </div>
+                    <Skill skills={data.skills.integration} />
                 </GridItem>
 
                 <GridItem styles="col-4">
                     <HeadingSecondary text="Infrastructure" />
 
-                    <div className="skills">
-                        <ul>
-                            <li>Heroku</li>
-                            <li>AWS</li>
-                            <li>Netify</li>
-                        </ul>
-                    </div>
+                    <Skill skills={data.skills.infrastructure} />
+                </GridItem>
+            </GridContainer>
+
+            <GridContainer>
+                <GridItem styles="col-4">
+                    <HeadingSecondary text="API Specs" />
+
+                    <Skill skills={data.skills.api} />
                 </GridItem>
             </GridContainer>
         </Container>
