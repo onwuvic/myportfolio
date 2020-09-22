a2209bb6a2dae11797996b3b2df79b882a375d7a
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-09-22 09:35:02 +0100

    add real blog post

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -6,6 +6,7 @@ import GridItem from '../Grid/GridItem';
 import Section from '../Section/Section';
 import Container from '../Container/Container';
 import PageHeader from '../PageHeader/PageHeader';
+import * as data from '../../data/index.json'
 
 const Blog = () => (
     <Section>
@@ -15,47 +16,19 @@ const Blog = () => (
             <GridContainer>
                 <HeadingSecondary text="Latest Articles" />
 
-                <GridItem styles="col-12">
-                    <h4 className="heading-sub">FEBRUARY 01, 2020</h4>
-                    <h2 className="title">
-                        <Link to={"#"} className="title__link">
-                            Tailwind CSS static navbar with shadow on scroll for Vue applications
-                </Link>
-                    </h2>
-                    <p className="paragraph">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Praesentium neque ea dolore culpa ex officiis et inventore recusandae quaerat?
-                        Itaque quos enim nulla nemo est officia suscipit mollitia quis voluptatum.
-              </p>
-                </GridItem>
-
-                <GridItem styles="col-12">
-                    <h4 className="heading-sub">JANUARY 04, 2020</h4>
-                    <h2 className="title">
-                        <Link to={"#"} className="title__link">
-                            Tailwind CSS static navbar with shadow on scroll for Vue applications
-                </Link>
-                    </h2>
-                    <p className="paragraph">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Praesentium neque ea dolore culpa ex officiis et inventore recusandae quaerat?
-                        Itaque quos enim nulla nemo est officia suscipit mollitia quis voluptatum.
-              </p>
-                </GridItem>
-
-                <GridItem styles="col-12">
-                    <h4 className="heading-sub">NOVEMBER 09, 2019</h4>
-                    <h2 className="title">
-                        <Link to={"#"} className="title__link">
-                            Tailwind CSS static navbar with shadow on scroll for Vue applications
-                </Link>
-                    </h2>
-                    <p className="paragraph">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Praesentium neque ea dolore culpa ex officiis et inventore recusandae quaerat?
-                        Itaque quos enim nulla nemo est officia suscipit mollitia quis voluptatum.
-              </p>
-                </GridItem>
+                {
+                    data.blogs.map(blog => {
+                        return <GridItem styles="col-12">
+                                    <h4 className="heading-sub">{blog.date}</h4>
+                                    <h2 className="title">
+                                        <a target="_blank" rel="noopener noreferrer" href={blog.link} className="title__link">
+                                            {blog.title}
+                                        </a>
+                                    </h2>
+                                    <p className="paragraph"> {blog.body} </p>
+                                </GridItem>
+                    })
+                }
             </GridContainer>
         </Container>
     </Section>
