5f0561359a145c31f3bfa7305a3bfe4edd8c3c6a
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-09-24 18:21:14 +0100

    redesign menu navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,45 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-const Header = () => (
-    <header className="header">
-        <div className="header__logo-box">
-            <Link to={"/"}>
-                <div className="header__logo-symbol">V</div>
-                <div className="header__logo-text">
-                    Victor <span>Onwuzor</span>
-                </div>
-            </Link>
-        </div>
+const Header = () => {
+    const [isClose, setActive] = useState(true);
 
-        <div className="header__navigation">
-            {/* <i className="header__navigation-icon">ICON</i> */}
-            <FontAwesomeIcon className="header__navigation-icon" icon={faBars} size="3x" />
-            {/* <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
-            <label className="navigation__button">
-                <span className="navigation__icon"></span>
-            </label>
-            <div className="navigation__background"></div> */}
-            <nav className="header__navigation-nav">
-                <ul className="header__navigation-list">
-                    <li className="header__navigation-item">
-                        <NavLink exact to="/" className="header__navigation-link">About Me</NavLink>
-                    </li>
-                    <li className="header__navigation-item">
-                        <NavLink to="/resume" className="header__navigation-link">Resume</NavLink>
-                    </li>
-                    <li className="header__navigation-item">
-                        <NavLink to={"/blog"} className="header__navigation-link">Blog</NavLink>
-                    </li>
-                    <li className="header__navigation-item">
-                        <NavLink to={"/contact"} className="header__navigation-link">Contact</NavLink>
-                    </li>
-                </ul>
-            </nav>        
-        </div>
-    </header>
-);
+    const toggleClass = () => {
+        setActive(!isClose);
+    };
+    
+    return (
+        <header className="header">
+            <div className="header__logo-box">
+                <Link to={"/"}>
+                    <div className="header__logo-symbol">V</div>
+                    <div className="header__logo-text">
+                        Victor <span>Onwuzor</span>
+                    </div>
+                </Link>
+            </div>
+    
+            <div className="header__navigation">
+                <FontAwesomeIcon className="header__navigation-icon" icon={faBars} size="3x" onClick={toggleClass} />
+        
+                {/* Mobile side nav */}
+                <div className={`side__nav animate ${isClose ? 'mobile-menu-hide': ''}`}>
+                    The side nav
+                </div>
+                
+                <nav className="header__navigation-nav">
+                    <ul className="header__navigation-list">
+                        <li className="header__navigation-item">
+                            <NavLink exact to="/" className="header__navigation-link">About Me</NavLink>
+                        </li>
+                        <li className="header__navigation-item">
+                            <NavLink to="/resume" className="header__navigation-link">Resume</NavLink>
+                        </li>
+                        <li className="header__navigation-item">
+                            <NavLink to={"/blog"} className="header__navigation-link">Blog</NavLink>
+                        </li>
+                        <li className="header__navigation-item">
+                            <NavLink to={"/contact"} className="header__navigation-link">Contact</NavLink>
+                        </li>
+                    </ul>
+                </nav>        
+            </div>
+        </header>
+    );
+}
 
 export default Header;
