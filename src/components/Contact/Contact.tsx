b28ad9d856985a1b35ae458c252080b9437ec65b
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-09-24 14:13:33 +0100

    add contact component style

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,15 +4,25 @@ import GridItem from '../Grid/GridItem';
 import Container from '../Container/Container';
 import PageHeader from '../PageHeader/PageHeader';
 import Section from '../Section/Section';
+import HeadingSecondary from '../Headings/HeadingSecondary';
 
 const Contact = () => (
-    <Section>
+    <Section styles="u-no-padding-bottom">
         <PageHeader title="Contact" />
 
         <Container>
             <GridContainer>
                 <GridItem styles="col-12">
-                    <p>Contact</p>
+                    <div className="contact">
+                        <HeadingSecondary text="Get in Touch" />
+
+                        <div className="contact__box">
+                            <h2 className="paragraph-lg u-margin-bottom-small">Want to work together or have any questions?</h2>
+                            <a href="mailto:victor.onwuzor@gmail.com" className="btn btn__primary">
+                                Say Hello <span role="img" aria-label="emoji">👋</span>
+                            </a>
+                        </div>
+                    </div>
                 </GridItem>
             </GridContainer>
         </Container>
