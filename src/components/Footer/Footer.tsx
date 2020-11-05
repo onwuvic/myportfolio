f3b21791b904b5d8c4c4f662e3dcd98d2c4e3de1
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-11-06 00:58:28 +0100

    Footer component scope style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faTwitter, faLinkedinIn, faMediumM, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
+import './Footer.scss';
 
 const Footer = () => (
     <footer className="footer">
